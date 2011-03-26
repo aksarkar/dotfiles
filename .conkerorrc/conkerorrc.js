58d7ab0436f8e1398f725bc64c9d99c0d34ce0f3
Abhishek Sarkar <asarkar@cs.unc.edu>
2011-03-26 17:24:17 -0400

    Hack to avoid bug

--- .conkerorrc/conkerorrc.js
+++ .conkerorrc/conkerorrc.js
@@ -25,8 +25,8 @@ define_webjump("blackboard", "https://blackboard.unc.edu/webapps/login/");
 define_webjump("compgenwiki",
                "http://compgen.unc.edu/wiki/index.php/Special:Search?search=%s",
                $alternative = "http://www.compgen.unc.edu/wiki");
-define_webjump("431", "http://www.cs.unc.edu/~jeffay/courses/comp431/");
-define_webjump("790", "http://cs.unc.edu/Courses/comp790-090-s11/");
+define_webjump("431 ", "http://www.cs.unc.edu/~jeffay/courses/comp431/");
+define_webjump("790 ", "http://cs.unc.edu/Courses/comp790-090-s11/");
 
 // Useful lookups
 define_webjump("archwiki", 
