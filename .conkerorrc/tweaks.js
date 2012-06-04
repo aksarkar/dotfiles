c68fcca80a91ce3f260ca5b1ed24490b1494ffee
Abhishek Sarkar <aksarkar@mit.edu>
2012-06-04 14:25:33 -0400

    Update homepage

--- .conkerorrc/tweaks.js
+++ .conkerorrc/tweaks.js
@@ -1,6 +1,6 @@
 register_user_stylesheet("file:///home/asarkar/.conkerorrc/content.css");
 
-homepage = "http://mit.edu/aksarkar";
+homepage = "http://mit.edu/aksarkar/www";
 
 can_kill_last_buffer = false;
 
