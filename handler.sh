904c305bb4527b4338f800fc9c9ef2148d39e4fc
Abhishek Sarkar <aksarkar@mit.edu>
2012-06-12 14:16:49 -0400

    Add support for docking station

--- handler.sh
+++ handler.sh
@@ -13,18 +13,46 @@ fi
 export DISPLAY=:0
 export XAUTHORITY=$XAUTH
 
+# Hack to detect whether or not we're docked. Thinkpad Mini Dock Plus Series 3
+# doesn't show up in sysfs as a dock, so test for the extra USB ports instead
+dockusb=/sys/devices/pci0000:00/0000:00:1a.0/usb1/1-1/1-1.5
+
 function lock {
     xautolock -locknow
 }
 
+function setbg {
+    eval $(cat /home/asarkar/.fehbg)
+}
+
+function dock {
+    xrandr --output LVDS1 --off
+    xrandr --output DP3 --auto --primary --output HDMI2 --auto --right-of DP3
+    setbg
+}
+
+function undock {
+    xrandr --output DP3 --off --output HDMI2 --off
+    xrandr --output LVDS1 --auto
+    setbg
+}
+
 function standby {
+    mount | awk '$5 == "fuse.sshfs" {print $3}' | parallel umount
     lock
     pm-suspend
+    # Make sure video outputs are correct on resume
+    if [[ -d $dockusb ]]
+    then
+        dock
+    else
+        undock
+    fi
 }
 
 case $1 in
     button/lid)
-        if [[ $3 = "close" ]]
+        if [[ $3 == "close" &&  ! -d $dockusb ]]
         then
             standby
         fi
@@ -37,5 +65,17 @@ case $1 in
         ;;
     button/f24)
         xinput set-int-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 8 0
+        ;;
+    ibm/hotkey)
+        if [[ $2 == IBM0068:00 ]]
+        then
+            if [[ $4 == 00004010 ]]
+            then
+                dock
+            elif [[ $4 == 00004011 ]]
+            then
+                standby
+            fi
+        fi
         ;;
 esac
