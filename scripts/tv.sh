034390180b1d72cfd1766e70e92f3746a41fb141
Abhishek Sarkar <aksarkar@mit.edu>
2013-07-19 19:20:48 -0400

    Helper script to connect to TV
    Send audio/video over HDMI. Audio needs to be sent to non-default ALSA device,
    so change the default in ~/.asoundrc as needed.

---
+++ scripts/tv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+pkill -USR1 redshift
+case $(cat /sys/class/drm/card0-HDMI-A-1/status) in
+    connected)
+        xrandr --output HDMI1 --auto --right-of LVDS1
+        ln -s $HOME/.dotfiles/hdmi.asoundrc $HOME/.asoundrc
+        ;;
+    disconnected)
+        xrandr --output HDMI1 --off
+        rm -f $HOME/.asoundrc
+        ;;
+esac
