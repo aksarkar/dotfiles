cd7dc3a8b206c728321362c8c80b9e7a872e67d5
Abhishek Sarkar <aksarkar@mit.edu>
2013-07-19 19:28:59 -0400

    Fix font sizes
    Update to freetype2-ubuntu broke these

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -56,7 +56,7 @@ manageHook' = composeAll
               , className =? "mplayer2" --> doFloat
               ]
 
-dmenu' = "dmenu_run -fn Consolas-7:bold -nb \\#002b36 -nf \\#93a1a1 -sb \\#073642 -sf \\#93a1a1"
+dmenu' = "dmenu_run -fn Consolas:bold:pixelsize=12 -nb \\#002b36 -nf \\#93a1a1 -sb \\#073642 -sf \\#93a1a1"
 
 config' = withUrgencyHook NoUrgencyHook defaultConfig
           { focusFollowsMouse = False
