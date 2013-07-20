beb5ea6be5885cbeb91b30c0bc5eeea9b7e494ae
Abhishek Sarkar <aksarkar@mit.edu>
2013-07-20 17:46:45 -0400

    Toggle screen locking when toggling TV display

--- scripts/tv.sh
+++ scripts/tv.sh
@@ -5,9 +5,11 @@ case $(cat /sys/class/drm/card0-HDMI-A-1/status) in
     connected)
         xrandr --output HDMI1 --auto --right-of LVDS1
         ln -s $HOME/.dotfiles/hdmi.asoundrc $HOME/.asoundrc
+        lc.sh
         ;;
     disconnected)
         xrandr --output HDMI1 --off
         rm -f $HOME/.asoundrc
+        lc.sh 1
         ;;
 esac
