4394504d5900d4e928dcb135cfc0151cf4a2004d
Abhishek Sarkar <aksarkar@mit.edu>
2013-10-10 00:09:31 -0400

    Simplify mount checking

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-[[ ! -z $(mount | grep "/media/backup") ]] || mount /media/backup
+mount | grep -q "/media/backup" || mount /media/backup
 mbsync -aq
 mu index --quiet --maildir=$HOME/mail
 rdiff-backup --exclude '**[cC]ache' --exclude "**downloads" /home/asarkar /media/backup/asarkar
