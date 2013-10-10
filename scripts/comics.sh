8e6c508380d2f5da8ead2f255b88dd9b06370bba
Abhishek Sarkar <aksarkar@mit.edu>
2013-10-10 00:10:28 -0400

    Replace for loop with parallel
    Update comics

--- scripts/comics.sh
+++ scripts/comics.sh
@@ -4,11 +4,7 @@ xs=("http://m.xkcd.com"
     "http://thisisindexed.com"
     "http://phdcomics.com"
     "http://survivingtheworld.net"
-    "http://euge.ca"
     "http://satwcomic.com"
+    "http://zenpencils.com"
    )
-
-for x in ${xs[@]}
-do
-    conkeror $x
-done
+parallel -X conkeror ::: ${xs[*]}
