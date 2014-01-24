c489eacf839011f1daa8920a19b27579edc21a5f
Abhishek Sarkar <aksarkar@mit.edu>
2014-01-23 23:38:24 -0500

    Clean up

--- scripts/comics.sh
+++ scripts/comics.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-xs=("http://m.xkcd.com"
-    "http://what-if.xkcd.com/"
-    "http://thisisindexed.com"
+xs=(
+    "http://euge.ca"
+    "http://m.xkcd.com"
     "http://phdcomics.com"
-    "http://survivingtheworld.net"
     "http://satwcomic.com"
+    "http://survivingtheworld.net"
+    "http://thisisindexed.com"
+    "http://what-if.xkcd.com/"
     "http://zenpencils.com"
    )
 parallel -j1 -X conkeror ::: ${xs[*]}
