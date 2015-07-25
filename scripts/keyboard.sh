9eeb578a33dec3e6fb9ba5523472eda0d597211e
Abhishek Sarkar <aksarkar@mit.edu>
2015-07-25 00:29:36 -0400

    Update keyboard mappings

--- scripts/keyboard.sh
+++ scripts/keyboard.sh
@@ -2,22 +2,26 @@
 set -e
 set -u
 
-auth=$(pgrep -a X | awk '{sub(/.*-auth/, "", $0); print $1}')
+auth=$(pgrep -a Xorg | awk '{sub(/.*-auth/, "", $0); print $1}')
 export XAUTHORITY=${auth?"failed to get XAuthority"}
 export DISPLAY=:0
 
+xkbset exp =st -twokey =latchlock
+xkbset st -twokey latchlock
+
 xmodmap -pk | grep -q "Alt_R" && xmodmap - <<EOF
-keysym Alt_R = Multi_key
-keysym Menu = Hyper_R
 keysym Super_R = Hyper_R
+keysym Menu = Multi_key
 EOF
 
 xmodmap - <<EOF
+clear control
 clear Mod1
 clear Mod3
 clear Mod4
 
-add Mod1 = Alt_L
+add control = Control_L Alt_R
+add Mod1 = Alt_L Control_R
 add Mod3 = Hyper_L Hyper_R
 add Mod4 = Super_L
 EOF
