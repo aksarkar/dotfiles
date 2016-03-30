402f5cb4fb067009d878d2a787c47094dc6c8879
Abhishek Sarkar <aksarkar@mit.edu>
2016-03-30 19:47:19 -0400

    Bump up font size

--- scripts/toggle.sh
+++ scripts/toggle.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i '1s/LIGHT/FOO/; 1s/DARK/LIGHT/; 1s/FOO/DARK/; 2s/8.5/0/; 2s/12/8.5/; 2s/0/12/' $HOME/.Xresources
+sed -i '1s/LIGHT/FOO/; 1s/DARK/LIGHT/; 1s/FOO/DARK/' $HOME/.Xresources
 xrdb merge $HOME/.Xresources
