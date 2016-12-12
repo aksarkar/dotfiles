884351397fe0a1a4e84e7a6857fe6e4ff818592b
Abhishek Sarkar <aksarkar@mit.edu>
2016-12-11 21:31:08 -0500

    Make policy apply to domains

--- .conkerorrc/policy.js
+++ .conkerorrc/policy.js
@@ -13,13 +13,16 @@ my_whitelist[11] = [
     "google.com",
 ];
 
-function whitelist(type, location, ...args) {
-    dumpln(`*** Checking ${type} ${location.host} `)
+function whitelist(type, location, origin, ...args) {
+    dumpln(`*** Checking ${type} ${location.host} ${origin.host}`)
     if (type in my_whitelist &&
-        my_whitelist[type].map(x => String.endsWith(location.host, x)).some(x => x)) {
+        my_whitelist[type].map(x => String.endsWith(location.host, x)).some(x => x) &&
+        String.endsWith(origin.host, location.host)) {
+        dumpln('Accepting')
         return content_policy_accept;
     }
     else {
+        dumpln('Rejecting')
         return content_policy_reject;
     }
 }
