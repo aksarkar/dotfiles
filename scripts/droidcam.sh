4157f47cf6fa4b65aa84e205c261e4a1aaa9d00f
Abhishek Sarkar <aksarkar@alum.mit.edu>
2023-12-09 14:55:35 -0600

    Make droidcam res configurable
    This is needed for Skype, which does not support 1080p webcam input

--- scripts/droidcam.sh
+++ scripts/droidcam.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo modprobe v4l2loopback video_nr=9 card_label=DroidCam
 adb forward tcp:4747 tcp:4747
-ffmpeg -i http://localhost:4747/mjpegfeed?1920x1080 -c:v rawvideo -vf format=yuv420p -f v4l2 /dev/video9
+ffmpeg -i http://localhost:4747/mjpegfeed?{$1:-1920x1080} -c:v rawvideo -vf format=yuv420p,vflip -f v4l2 /dev/video9
